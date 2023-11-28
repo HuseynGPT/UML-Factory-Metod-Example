b4249fc36ec3f4663464b6b4a02686ffac052819
Huseynov Huseyn Babek <Husey_lo34@itstep.edu.az>
2023-11-28 11:01:32 +0400

    uml

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,66 @@
+﻿abstract class Dialog
+{
+    public void render()
+    {
+        IButton okButton = createButton();
+        okButton.onClick();
+        okButton.render();
+    }
+
+    public virtual IButton createButton()
+    {
+        return null;
+    }
+
+}
+
+interface IButton
+{
+    void render();
+    void onClick();
+
+}
+
+
+class WindowsDialog:Dialog
+{
+    public override IButton createButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+class WebDialog : Dialog
+{
+    public override IButton createButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+class WindowsButton : IButton
+{
+    public void onClick()
+    {
+
+        // code
+    }
+
+    public void render()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class HTMLButton : IButton
+{
+    public void onClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void render()
+    {
+        throw new NotImplementedException();
+    }
+}
